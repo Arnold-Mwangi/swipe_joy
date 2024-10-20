e6c8d1e9e20de7a62290a9cf3ef69f2698b0c627
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-10-20 22:12:12 +0300

    setting up tlelgream detection

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { ThemeToggle } from './ThemeToggle';
 
 export default function Home() {
   const [isAuthorized, setIsAuthorized] = useState(false);
